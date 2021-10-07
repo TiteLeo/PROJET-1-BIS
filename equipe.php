5033a6bfc03914a8f272f0761f9510e3c445c410
Eleonore <eleonoregouze33@gmail.com>
2021-10-07 11:36:47 +0200

    Modifications margin and padding team

--- equipe.php
+++ equipe.php
@@ -6,10 +6,11 @@
         <p class=" text-justify">   
         <p>Voici notre équipe de proximité, toujours la pour votre service !</p>
         <p>L'équipe de choc vous accueille du Lundi au Dimanche,
-            de 8h à 16h (sobre de 8h à 8h45...).
-            Téléphone: 05 40 12 15 33 ou pas 
-            13 All. Serr, 33000 Bordeaux
+        <p>de 8h à 16h (sobre de 8h à 8h45...).</p>
+        <p>Téléphone: 05 40 12 15 33 ou pas</p>
+        <p>13 All. Serr, 33000 Bordeaux</p>
         </p>
+    <br>
     </div>
     <div id="map"><img src="PHOTOS/plan.png" alt="plan"></div>
     <div id="picture-team"><img id="image-team" src="./PHOTOS/equipe.jpg" alt="Notre équipe"></div>
