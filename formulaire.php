38c05b295a01071afe3b506d119b9b31c4890fee
Brice <officielbrice@gmail.com>
2021-10-07 10:53:23 +0200

    add hotel and restaurant page

--- formulaire.php
+++ formulaire.php
@@ -4,41 +4,17 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width" />
     <title>Commissariat</title> 
+      <link href="hotel.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&display=swap" rel="stylesheet">
     />
-    <link href="style.css" rel="stylesheet" type="text/css" />
+    <link href="hotel.css" rel="stylesheet" type="text/css" />
   </head>
 
   <body>
-    <section id="contact">
-      <h1>Formulaire de délit</h1>
-      <p></p>
-      <form id="contactForm">
-        <label for="firstname">First name:</label><br />
-        <input
-          type="text"
-          id="firstname"
-          name="firstname"
-          value="Votre prénom"
-        /><br />
-        <label for="lastname">Last name:</label><br />
-        <input type="text" id="lastname" name="lastname" value="Votre nom" /><br />
-        <label for="email">Email:</label><br />
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value="faites_nous_confiance@jailhouse.com"
-        /><br />
-        <label for="message">Expliquez nous tranquillement votre délit:</label><br />
-        <textarea id="message" name="message"></textarea>
-        <br /><br />
-        <input type="submit" value="Submit" />
-      </form>
-    </section>
+   
 
-    <script src="script.js"></script>
+          
   </body>
 </html>
---
+++ hotel.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>Hotel et Restaurant</title> 
+      <link href="hotel.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&display=swap" rel="stylesheet">
+    />
+    <link href="hotel.css" rel="stylesheet" type="text/css" />
+  </head>
+
+  <body>
+      <section class="enhaut" id="haut">
+         
+       <h1>Hotel et Restauration</h1>
+        <h4>Un commissariat aux multiples facettes</h4>
+         
+      </section>
+      
+
+      
+    <section class="hotres" id="hotel">
+       
+         <div class ="recep">
+            <div class="chamber">
+               <div class="left">
+                 <h1> Chambres conviviales </h1>
+                   <p>C’est à l’unisson que tous nos services feront de votre séjour une expérience hors-du-commun.
+    Signées Jacques Garcia, les 110 Chambres et Suites de l’hôtel du Commissariat de la Wild possèdent un cachet unique, vous offrent un confort exceptionnel ainsi qu’un emplacement unique au sein Bordeaux.
+                   </p>
+                </div>
+                <div class="right">
+                   <img src="PHOTOS/bunks-4899875_1920.resized.jpg" alt="">
+                   </div>
+                </div>
+                <div class="chamber">
+                  <div class="left">
+                     <h1> Chambres uniques </h1>
+                      <p>Avec un espace très généreux de 35 m² et une décoration élégante, les Chambres Deluxe mêlent confort, raffinement et modernité par un habile jeu de couleurs et de textures. La fusion parfaite entre contemporain et histoire en fait l’endroit rêvé pour se détendre après une journée à découvrir le charme indiscutable de Bordeaux.
+                   </p>
+                    </div>
+                <div class="right">
+                   <img src="PHOTOS/cellulesolo.jpg" alt="">
+                   </div>
+           </div>
+        </div>
+      </section>
+
+        
+    <section class="restau" id="restaurant">
+    <div class ="miam">
+        <p>L’équipe du restaurant est ravie de vous faire découvrir le nouveau menu et la nouvelle carte de ce lieu devenu une table incontournable de la haute gastronomie dans la capitale Girondine. Dégustez des mets d’excellence accompagnés des plus grands vins sélectionnés par nos Chefs et Sommeliers. Dans une ambiance conviviale et feutrée, chaque moment est intense, riche et mémorable.</p>
+         </div>
+      </section>
+      <section class="buffet" id="buffet">
+       <div class="eat">
+           <div class="left">
+              <h1>Notre buffet</h1>
+               <p>Le restaurant du Comissariat se définit par la simplicité de ses plats, rappelant la cuisine française de nos mères et grands-mères. Ainsi que par la qualité de ses produits frais, sélectionnés avec attention et cuisinés avec goût. Mangez sainement mais délicieusement. Nos clients sont rois.</p>
+           </div>
+         <div class="right">
+           <img src="PHOTOS/burger.png" alt="">
+           </div>
+          </div>
+        
+      </section>
+
+          
+  </body>
+</html>
